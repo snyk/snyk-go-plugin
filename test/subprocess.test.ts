ab4b91df10a97160a9e67c91e37952b7328243cf
Jason Luong <jason.luong@snyk.io>
2023-03-22 18:17:08 +0000

    WIP: testing

--- test/subprocess.test.ts
+++ test/subprocess.test.ts
@@ -1,19 +1,24 @@
-import {test} from 'tap';
+import { test } from 'tap';
+import { execute } from '../lib/sub-process';
 
 test('sub-process', (t) => {
-  t.test('restore proxy environemnt', async (t) => {
-    let preTestEnv = {...process.env}
+  t.test('restore proxy environment', async (t) => {
+    let preTestEnv = { ...process.env };
     
-    process.env.SNYK_SYSTEM_HTTPS_PROXY = "http://127.0.0.1"
-    process.env.SNYK_SYSTEM_HTTP_PROXY = "http://127.0.0.1"
-    process.env.SNYK_SYSTEM_NO_PROXY = "example.com"
-
-    process.env.SNYK_SYSTEM_HTTPS_PROXY = "http://127.0.0.1"
-    process.env.SNYK_SYSTEM_HTTP_PROXY = "http://127.0.0.1"
-    process.env.SNYK_SYSTEM_NO_PROXY = "example.com"
-
+    process.env.SNYK_SYSTEM_HTTPS_PROXY = "http://127.0.0.1";
+    process.env.SNYK_SYSTEM_HTTP_PROXY = "http://127.0.0.1";
+    process.env.SNYK_SYSTEM_NO_PROXY = "example.com";
+    
+    // process.env.SNYK_SYSTEM_HTTPS_PROXY = "http://127.0.0.1";
+    // process.env.SNYK_SYSTEM_HTTP_PROXY = "http://127.0.0.1";
+    // process.env.SNYK_SYSTEM_NO_PROXY = "example.com";
+    
     // restore env before test
-    process.env = {...preTestEnv}
+    process.env = { ...preTestEnv };
+    
+    await execute('test', ['-d']);
+    t.deepEquals(process.env, preTestEnv);
   });
 
-})
+  t.end();
+});
